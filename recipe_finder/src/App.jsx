c8182a1090a6a3a7fa04d7394a2f712735f8cf82
Daniel Chin <danielchin@Daniels-MacBook-Pro.local>
2024-12-05 02:21:32 +0900

    worked on testing app

--- recipe_finder/src/App.jsx
+++ recipe_finder/src/App.jsx
@@ -1,16 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
 
   return (
     <>
-
+      <div className="App">
+        <h1>Counter App</h1>
+        <p>Current Count: {count}</p>
+        <button onClick={increment}>Increase</button>
+      </div>
     </>
-  )
+  );
 }
 
 export default App
